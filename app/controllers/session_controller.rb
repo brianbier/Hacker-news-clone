518bb112a2c4b1285284438a312f5ad6a099bdec
Brian Bier <briansbier@gmail.com>
2016-04-04 22:06:31 -0400

    Include user delete forms and views for login in and login out

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,46 @@
+#Index Page
+get '/' do 
+  erb :'/'
+end
+
+#sign in page 
+get '/login' do
+  erb :'/login'
+end
+
+#sign in user
+post '/login' do
+  user = User.find_by(email: params[:user][:email])
+  if user && user.authenticate(params[:user][:password])
+    session[:user_session_id] = user.id
+    redirect '/' #Requires index to where to redirect
+  else
+    @errors = ["Wrong username or password"]
+    erb :'sessions/new'
+  end
+end
+
+
+#sign up page
+get '/users/new' do
+  erb :'/new'
+end
+
+#Create new User
+post '/users' do
+ @user = User.new(params[:user])
+   if @user.save
+    session[:user_id] = @user.id
+
+     redirect "/users/#{@user.id}"
+   else
+    @errors = user.errors.full_messages
+    erb :'users/new'
+   end
+end
+
+#User delete session
+delete '/logout' do
+  session.clear
+  redirect '/'
+end
