6c35a7b8db43f039b02c8d1c068a9315b98944de
Brian Bier <briansbier@gmail.com>
2016-04-05 10:47:31 -0400

    Create tables and models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true,uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
   has_secure_password
+  has_many :posts
+  has_many :comments
 end
--- db/migrate/20160404213057_create_users.rb
+++ db/migrate/20160404213057_create_users.rb
@@ -1,9 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :first_name, null: false
-      t.string :last_name, null: false
-      t.string :email, null: false, unique: true, index: true
+      t.string :user_name, null: false, unique: true, index: true
+      t.string :email, null: false, unique: true
       t.string :password_digest, null: false
 
       t.timestamps null: false
