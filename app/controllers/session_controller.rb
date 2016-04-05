b073662610395af499855059f5f746ce302ed2d1
Brian Bier <briansbier@gmail.com>
2016-04-04 22:11:38 -0400

    Minor changes to the new page and the session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -31,7 +31,6 @@ post '/users' do
  @user = User.new(params[:user])
    if @user.save
     session[:user_id] = @user.id
-
      redirect "/users/#{@user.id}"
    else
     @errors = user.errors.full_messages
