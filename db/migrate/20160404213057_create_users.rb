a7baaf8afb505d0d6d8f4e9218dce335de9b31a6
Brian Bier <briansbier@gmail.com>
2016-04-04 21:52:23 -0400

    Include time stamps for users table

--- db/migrate/20160404213057_create_users.rb
+++ db/migrate/20160404213057_create_users.rb
@@ -3,8 +3,10 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.string :email, null: false, unique: true
+      t.string :email, null: false, unique: true, index: true
       t.string :password_digest, null: false
+
+      t.timestamps null: false
     end
   end
 end
